e24403aae96eebfa05a790fc1ae9a0088041be07
風見幽香 <PatheticFish@users.noreply.github.com>
2017-07-11 12:45:34 -0700

    add dimensions check tests

--- pokeredmaps.Test.ps1
+++ pokeredmaps.Test.ps1
@@ -2,6 +2,7 @@
     $tilesetlist = (Get-Content .\tilesetlist.txt) -split "`n"
     $dataregex = [regex]"`tdb\s(\w+)"
     $MapNameDictionary = Import-Csv "$scriptdir\mapnamedictionary.csv"
+    $MapDimensionsTable = Import-Csv "$scriptdir\mapdimensions.csv"
 
     Context "Tilesets" {
         $MapNameDictionary | ForEach-Object {
@@ -16,6 +17,20 @@
                 }
                 $correctTileset = $tilesetlist.IndexOf($dataregex.Match((Get-Content .\pokered\data\mapHeaders\$map.asm)).Groups[1].Value)
                 $tileset | Should Be $correctTileset
+            }
+        }
+    }
+
+    Context "Dimensions" {
+        $MapNameDictionary | ForEach-Object {
+            $map = $_.CONSTANT
+            if ($map.Contains("UNUSED")) { return }
+            It "obtains the correct dimensions for $map" {
+                $headerobject = mapheaders $map
+                $entry = $MapDimensionsTable | Where {$_.MAP -eq $map}
+
+                $headerobject.width | Should Be $entry.WIDTH
+                $headerobject.height | Should Be $entry.HEIGHT
             }
         }
     }
